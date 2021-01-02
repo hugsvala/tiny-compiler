94d5f415173186684a0b2715ec8deccfd83281e1
es <erik.samuelsson@protonmail.com>
2021-01-02 23:26:05 +0100

    negative numbers

--- lexer.py
+++ lexer.py
@@ -6,9 +6,9 @@ import sys
 
 # This pattern define the current subset of the language. Currently it only
 # operates on integers.
-language = '[a-zA-Z][a-zA-Z0-9_]*|[0-9]+|[,;(){}+\-*/=!<>]'
+language = '[a-zA-Z][a-zA-Z0-9_]*|-?[0-9]+|[,;(){}+\-*/=!<>]'
 id = '[a-zA-Z][a-zA-Z0-9_]*'
-literal = '[0-9]+'
+literal = '-?[0-9]+'
 valid_tokens = { "int" : "int", "return" : "return", "if" : "if",
                  "else" : "else", "(" : "left_paren", ")" : "right_paren",
                  "{" : "left_bracket", "}" : "right_bracket", ";" :
@@ -27,12 +27,12 @@ def scan():
 
     # Once the input is scanned, we loop through and evaluate the result.
     for w in words:
-        if valid_tokens.get(w):
+        if m := re.match(literal, w):
+                tokens.append(("literal", m.group(0)))
+        elif valid_tokens.get(w):
             tokens.append((valid_tokens[w], w))
         elif m := re.match(id, w):
                 tokens.append(("id", m.group(0)))
-        elif m := re.match(literal, w):
-                tokens.append(("literal", m.group(0)))
 
     tokens.append(("eof", "$"))
     return tokens
