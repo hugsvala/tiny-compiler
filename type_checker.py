158892d9144af0f33da6eb195309d68b9d2792aa
es <erik.samuelsson@protonmail.com>
2021-01-03 23:35:31 +0100

    nbr locals

--- type_checker.py
+++ type_checker.py
@@ -19,6 +19,7 @@ class f_entry:
 @dataclass
 class v_entry:
     name : str
+    local_index : int
     type : str = "int"
 
 v_table = deque() # Variable symbol table, scopes are dicts.
@@ -53,13 +54,10 @@ def check_ftable_use(name, nbr_param):
 
 # Make sure that variables are declared before they are used.
 def check_vtable_use(name):
-    not_found = True
     for v in v_table:
         if v.get(name):
-            not_found = False
-            break
-    if not_found:
-        report_error("Variable used before declared.")
+            return v[name].local_index
+    report_error("Variable used before declared.")
 
 # Add a function to the fucntion symbol table.
 def add_f_entry(name, nbr_param):
@@ -68,10 +66,10 @@ def add_f_entry(name, nbr_param):
 
 # Add a variable to the current scope. For simplicity we don't allow shared
 # names between functions and variables.
-def add_v_entry(name):
+def add_v_entry(name, local_index):
     check_ftable_def(name)
     check_vtable_def(name)
-    v_table[0][name] = v_entry(name)
+    v_table[0][name] = v_entry(name, local_index)
 
 # Note that the parser doesn't currently parse global variables.
 def type_check(prog_ast):
@@ -84,15 +82,17 @@ def type_check(prog_ast):
 # Go through the function and look for defs and uses.
 def type_check_func(func):
     for param in func.params:
-        add_v_entry(param.name)
-    type_check_block(func.block)
+        add_v_entry(param.name, 0)
+    type_check_block(func, func.block)
 
-def type_check_stmt(stmt):
+# We pass func as an argument so that we can propagate it along to the
+# type_check_decl method, and update the nbr_locals used by codegen.
+def type_check_stmt(func, stmt):
     if stmt.node_type == "decl_node":
-        type_check_decl(stmt)
+        type_check_decl(func, stmt)
     elif stmt.node_type == "block_node":
         enter_scope()
-        type_check_block(stmt)
+        type_check_block(func, stmt)
         exit_scope()
     elif stmt.node_type == "assignment_node":
         check_vtable_use(stmt.name)
@@ -100,20 +100,22 @@ def type_check_stmt(stmt):
     elif stmt.node_type == "func_call_node":
         type_check_func_call(stmt)
     elif stmt.node_type == "if_node":
-        type_check_if_stmt(stmt)
+        type_check_if_stmt(func, stmt)
     elif stmt.node_type == "return_node":
         type_check_exp(stmt.exp)
 
 # The calling function make sure to enter a new scope for block, even though
 # type_check_block could do it, but this way is easier to scope params.
-def type_check_block(block):
+def type_check_block(func, block):
     for stmt in block.stmts:
-        type_check_stmt(stmt)
+        type_check_stmt(func, stmt)
 
 # First make sure that we haven't declared the name earlier in scope.
-def type_check_decl(decl):
+def type_check_decl(func, decl):
     decl_name = decl.name
-    add_v_entry(decl_name)
+    func.nbr_locals += 1
+    add_v_entry(decl_name, func.nbr_locals)
+    decl.local_index = func.nbr_locals
     if decl.exp:
         type_check_exp(decl.exp)
 
@@ -122,7 +124,7 @@ def type_check_func_call(func_call):
     for arg in func_call.args:
         type_check_exp(arg)
 
-def type_check_if_stmt(if_stmt):
+def type_check_if_stmt(func, if_stmt):
     condition = if_stmt.condition
     stmt = if_stmt.stmt
     opt_else = if_stmt.opt_else
@@ -131,25 +133,25 @@ def type_check_if_stmt(if_stmt):
         type_check_exp(condition.op2)
     if stmt.node_type == "block_node":
         enter_scope()
-        type_check_block(stmt)
+        type_check_block(func, stmt)
         exit_scope()
     else:
         enter_scope()
-        type_check_stmt(stmt)
+        type_check_stmt(func, stmt)
         exit_scope()
     if opt_else:
         if opt_else.node_type == "block_node":
             enter_scope()
-            type_check_block(opt_else)
+            type_check_block(func, opt_else)
             exit_scope()
         else:
             enter_scope()
-            type_check_stmt(opt_else)
+            type_check_stmt(func, opt_else)
             exit_scope()
 
 def type_check_exp(node):
     if node.name:
-        check_vtable_use(node.name)
+        node.local_index = check_vtable_use(node.name)
     elif node.is_exp:
         type_check_exp(node.op1)
         type_check_exp(node.op2)
